1a2fbd87571be635deeaeee2e9a699fc02c45c12
Svea Wilkens <svea.wilkens@elev.ga.ntig.se>
2026-05-07 10:52:35 +0200

    Har gjort responsiv layout och hemlig låda

---
+++ world/world.js
@@ -0,0 +1,8 @@
+
+function gomText(params) {
+    document.querySelector('.info-text').style.display = "none"
+    document.querySelector('.hemlig-lada').style.display = "none"
+}
+function visaText(params) {
+    document.querySelector('.hemlig-lada').style.display = "block"
+}
